7fc887b19de0446e6098fadbd0bb5b0ade85c4c1
Evgenii Mikhailov <evmikhaylov@gmail.com>
2023-03-12 19:14:02 +0100

    add thread safety test

--- LRUCache/LRUCache_test.cpp
+++ LRUCache/LRUCache_test.cpp
@@ -1,5 +1,6 @@
 #include "LRUCache.h"
 #include <gtest/gtest.h>
+#include <thread>
 
 TEST(LRUCacheTest, SetAndGet) {
     LRUCache::LRUCache<int, std::string> cache(2);
@@ -39,4 +40,41 @@ TEST(LRUCacheTest, Clear) {
     std::string value;
     EXPECT_FALSE(cache.Get(1, value));
     EXPECT_FALSE(cache.Get(2, value));
+}
+
+TEST(LRUCacheTest, ThreadSafetyTest) {
+    const int THREAD_COUNT = 10;
+    const int OPERATIONS_PER_THREAD = 100000;
+
+    LRUCache::LRUCache<int, int> cache(100);
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        threads.emplace_back([&]() {
+            for (int j = 0; j < OPERATIONS_PER_THREAD; ++j) {
+                int key = rand() % 1000;
+                if (rand() % 2 == 0) {
+                    int value = rand();
+                    cache.Set(key, value);
+                } else {
+                    int value;
+                    cache.Get(key, value);
+                }
+            }
+        });
+    }
+
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        threads[i].join();
+    }
+
+    int item_count = 0;
+    int value;
+    for (int i = 0; i < 1000; ++i) {
+        if (cache.Get(i, value)) {
+            item_count++;
+        }
+    }
+    EXPECT_LE(item_count, 100);
 }
